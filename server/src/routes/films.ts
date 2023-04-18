cd25b1302fb9fe29ca8446cf0459efc910606b70
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-04-18 19:14:53 -0300

    films and planets ready

--- server/src/routes/films.ts
+++ server/src/routes/films.ts
@@ -5,8 +5,8 @@ const routerFilm = Router();
 
 routerFilm.get("/", getFilm);
 routerFilm.get("/:id", getFilmById);
-routerFilm.put("/:id", updateFilm);
 routerFilm.post("/", postFilm);
+routerFilm.put("/:id", updateFilm);
 routerFilm.delete("/:id", deleteFilm);
 
 export {routerFilm};
--- server/src/routes/planets.ts
+++ server/src/routes/planets.ts
@@ -1,6 +1,13 @@
 import {Router}  from "express";
+import { createPlanet, deletePlanet, getPlanet, getPlanetById, updatePlanet } from "../controllers/planetsController";
 
 const routerPlanets = Router();
+
+routerPlanets.get("/", getPlanet);
+routerPlanets.get("/:id", getPlanetById);
+routerPlanets.post("/", createPlanet);
+routerPlanets.put("/:id", updatePlanet);
+routerPlanets.delete("/:id", deletePlanet);
 
 
 export {routerPlanets};
