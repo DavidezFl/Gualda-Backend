278e00c8eb26d3454e68c16fcd17a21440f357b0
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-04-19 07:11:44 -0300

    modified

--- server/index.ts
+++ server/index.ts
@@ -7,10 +7,9 @@ import { saveJsonDb } from "./src/utils/saveJsonDb";
 const PORT = process.env.PORT || 3001;
 
 connectDatabase().then(() => {
- //Upload JSON to MongoDb... //If you want you can comment when you are in dev mode after used it.
+ //Upload JSON to MongoDb... //If you want you can comment when you are in dev mode after used it 1 time.
   saveJsonDb();   
   app.listen(PORT, () =>{
   console.log(`Listening on http://localhost:${PORT}`);
-  
 })
 });
