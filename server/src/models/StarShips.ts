550ed280db4b85b803cb97571877d51e710dc3de
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-04-19 03:19:52 -0300

    Ready models

--- server/src/models/StarShips.ts
+++ server/src/models/StarShips.ts
@@ -3,11 +3,26 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const starShipSchema = new Schema(
-{
-
-}
+  {
+    name:{type: String, required: true},
+    model: String,
+    manufacturer: String,
+    cost_in_credits: String,
+    length: String,
+    max_atmosphering_speed: String,
+    crew: String,
+    passengers: String,
+    cargo_capacity: Number,
+    consumables: String,
+    hyperdrive_rating: Number,
+    MGLT: Number,
+    starship_class: String,
+    created: { type: Date, default: Date.now },
+  edited: { type: Date, default: Date.now },
+  },
+{ timestamps: false }
 );
 
-const StarShip = mongoose.model("StarShip", starShipSchema);
+export const StarShip = mongoose.model("StarShip", starShipSchema);
 
-export {StarShip};
+export const deleteStarShipById = (id: string) => StarShip.findOneAndDelete({ _id: id});
