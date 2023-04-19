5e412e4f6e1343640e96d27966622e990d44a353
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-04-19 03:20:48 -0300

    Ready Routes

--- server/src/routes/starShips.ts
+++ server/src/routes/starShips.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { createStarShip, deleteStarShip, getStarShip, getStarShipById, updateStarShip } from "../controllers/starShipsController";
 
 const routerStarShips = Router();
+
+routerStarShips.get("/", getStarShip);
+routerStarShips.get("/:id", getStarShipById);
+routerStarShips.post("/", createStarShip);
+routerStarShips.put("/:id", updateStarShip);
+routerStarShips.delete("/:id", deleteStarShip);
 
 export {routerStarShips};
