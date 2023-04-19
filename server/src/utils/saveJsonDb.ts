62987b943730105c9fd511a36973fab04b864205
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-04-19 03:24:03 -0300

    saving

--- server/src/utils/saveJsonDb.ts
+++ server/src/utils/saveJsonDb.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { Film } from "../models/Films";
 import { Planet } from "../models/Planets";
+import { StarShip } from "../models/StarShips";
 
 interface FilmsData {
   title: String,
@@ -15,9 +16,9 @@ interface FilmsData {
 
 interface PlanetsData {
   name: String,
-  rotation_period: String,
-  orbital_period: String,
-  diameter: String,
+  rotation_period: Number,
+  orbital_period: Number,
+  diameter: Number,
   climate: String,
   gravity: String,
   terrain: String,
@@ -26,6 +27,24 @@ interface PlanetsData {
   created: Date,
   edited: Date,
 }
+
+interface StarShipsData {
+  model: String,
+    manufacturer: String,
+    cost_in_credits: String,
+    length: String,
+    max_atmosphering_speed: String,
+    crew: String,
+    passengers: String,
+    cargo_capacity: Number,
+    consumables: String,
+    hyperdrive_rating: Number,
+    MGLT: Number,
+    starship_class: String,
+    created:Date,
+  edited:Date
+}
+
 
 const readJsonFile = async <T>(filePath: string): Promise<T[]> => {
   const jsonData = await fs.promises.readFile(filePath, "utf8");
@@ -38,6 +57,10 @@ const saveJsonDb = async () => {
   try {
     const films = await readJsonFile<FilmsData>("./dataJson/films.json");
     const planets = await readJsonFile<PlanetsData>("./dataJson/planets.json");
+    const starShips = await readJsonFile<StarShipsData>("./dataJson/starships.json");
+
+    await StarShip.deleteMany({});
+    await StarShip.insertMany(starShips);
 
     await Planet.deleteMany({});
     await Planet.insertMany(planets);
