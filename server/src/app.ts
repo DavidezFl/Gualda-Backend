799bcbb132204ee040fb20cb99ddf431fd7fed77
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-04-19 03:35:14 -0300

    App modified

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import  express  from "express";
+import  express, { NextFunction, Request, Response }  from "express";
 import cors from "cors";
 import { router } from "./routes";
 import morgan from "morgan";
@@ -12,5 +12,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/", router);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
 
 export {app};
